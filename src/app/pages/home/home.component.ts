8bdf4b22516090e775c3719f8c44bbd5df9c33a3
Jeffrey <77903214+jgrospe92@users.noreply.github.com>
2023-09-09 13:27:03 -0400

    updated carousel style

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -7,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit{
   ngOnInit(): void {
+    this.scrollRight();
     console.log('home page loaded');
+  }
+
+  @ViewChild('horizontal_scroll', { read: ElementRef }) public horizontal_scroll!: ElementRef<any>;
+
+  public scrollRight(): void {
+    this.horizontal_scroll.nativeElement.scrollTo({ left: (this.horizontal_scroll.nativeElement.scrollLeft + 150), behavior: 'smooth' });
+  }
+
+  public scrollLeft(): void {
+    this.horizontal_scroll.nativeElement.scrollTo({ left: (this.horizontal_scroll.nativeElement.scrollLeft - 150), behavior: 'smooth' });
   }
 
 
